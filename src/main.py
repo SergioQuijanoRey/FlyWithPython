37340309d8c27c3b404aa0a7b8796602742c7872
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-04-25 01:37:02 +0200

    feat: more functioning code added to the codebase

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import krpc
 import time
+import math
 
 class BasicSpaceShip:
     """
@@ -73,80 +74,128 @@ class OrbitalSpaceship(BasicSpaceShip):
 
         # TODO -- dirty code
         conn = self.connection
+        turn_start_altitude = 250
+        turn_end_altitude = 45000
+        target_altitude = 150000
 
-        # Point vertically and stabilize direction
-        vessel.auto_pilot.target_pitch_and_heading(90, 90)
+        conn = krpc.connect(name='Launch into orbit')
+        vessel = conn.space_center.active_vessel
+
+# Set up streams for telemetry
+        ut = conn.add_stream(getattr, conn.space_center, 'ut')
+        altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
+        apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+        stage_2_resources = vessel.resources_in_decouple_stage(stage=2, cumulative=False)
+        srb_fuel = conn.add_stream(stage_2_resources.amount, 'SolidFuel')
+
+# Pre-launch setup
+        vessel.control.sas = False
+        vessel.control.rcs = False
+        vessel.control.throttle = 1.0
+
+# Countdown...
+        print('3...')
+        time.sleep(1)
+        print('2...')
+        time.sleep(1)
+        print('1...')
+        time.sleep(1)
+        print('Launch!')
+
+# Activate the first stage
+        vessel.control.activate_next_stage()
         vessel.auto_pilot.engage()
-        vessel.control.throttle = 1
-        time.sleep(1)
+        vessel.auto_pilot.target_pitch_and_heading(90, 90)
 
-        print('Liftoff!')
-        vessel.control.activate_next_stage()
+# Main ascent loop
+        srbs_separated = False
+        turn_angle = 0
+        while True:
 
-        # Wait until fuel is consumed
-        fuel_amount = conn.get_call(vessel.resources.amount, 'SolidFuel')
-        expr = conn.krpc.Expression.less_than(
-            conn.krpc.Expression.call(fuel_amount),
-            conn.krpc.Expression.constant_float(0.1))
-        event = conn.krpc.add_event(expr)
-        with event.condition:
-            event.wait()
-        print('Booster separation')
-        vessel.control.activate_next_stage()
+            # Gravity turn
+            if altitude() > turn_start_altitude and altitude() < turn_end_altitude:
+                frac = ((altitude() - turn_start_altitude) /
+                        (turn_end_altitude - turn_start_altitude))
+                new_turn_angle = frac * 90
+                if abs(new_turn_angle - turn_angle) > 0.5:
+                    turn_angle = new_turn_angle
+                    vessel.auto_pilot.target_pitch_and_heading(90-turn_angle, 90)
 
-        # Wait until fuel is consumed -- second time
-        fuel_amount = conn.get_call(vessel.resources.amount, 'SolidFuel')
-        expr = conn.krpc.Expression.less_than(
-            conn.krpc.Expression.call(fuel_amount),
-            conn.krpc.Expression.constant_float(0.1))
-        event = conn.krpc.add_event(expr)
-        with event.condition:
-            event.wait()
-        print('Booster separation')
-        vessel.control.activate_next_stage()
+            # Separate SRBs when finished
+            if not srbs_separated:
+                if srb_fuel() < 0.1:
+                    vessel.control.activate_next_stage()
+                    srbs_separated = True
+                    print('SRBs separated')
 
-        # Reaching apoapsis
-        mean_altitude = conn.get_call(getattr, vessel.flight(), 'mean_altitude')
-        expr = conn.krpc.Expression.greater_than(
-            conn.krpc.Expression.call(mean_altitude),
-            conn.krpc.Expression.constant_double(10000))
-        event = conn.krpc.add_event(expr)
-        with event.condition:
-            event.wait()
+            # Decrease throttle when approaching target apoapsis
+            if apoapsis() > target_altitude*0.9:
+                print('Approaching target apoapsis')
+                break
+            # Disable engines when target apoapsis is reached
+        vessel.control.throttle = 0.25
+        while apoapsis() < target_altitude:
+            pass
+        print('Target apoapsis reached')
+        vessel.control.throttle = 0.0
 
-        print('Gravity turn')
-        vessel.auto_pilot.target_pitch_and_heading(60, 90)
+# Wait until out of atmosphere
+        print('Coasting out of atmosphere')
+        while altitude() < 70500:
+            pass
 
-        apoapsis_altitude = conn.get_call(getattr, vessel.orbit, 'apoapsis_altitude')
-        expr = conn.krpc.Expression.greater_than(
-            conn.krpc.Expression.call(apoapsis_altitude),
-            conn.krpc.Expression.constant_double(100000))
-        event = conn.krpc.add_event(expr)
-        with event.condition:
-            event.wait()
+# Plan circularization burn (using vis-viva equation)
+        print('Planning circularization burn')
+        mu = vessel.orbit.body.gravitational_parameter
+        r = vessel.orbit.apoapsis
+        a1 = vessel.orbit.semi_major_axis
+        a2 = r
+        v1 = math.sqrt(mu*((2./r)-(1./a1)))
+        v2 = math.sqrt(mu*((2./r)-(1./a2)))
+        delta_v = v2 - v1
+        node = vessel.control.add_node(
+            ut() + vessel.orbit.time_to_apoapsis, prograde=delta_v)
 
-        print('Launch stage separation')
-        vessel.control.throttle = 0
-        time.sleep(1)
-        vessel.control.activate_next_stage()
-        vessel.auto_pilot.disengage()
+# Calculate burn time (using rocket equation)
+        F = vessel.available_thrust
+        Isp = vessel.specific_impulse * 9.82
+        m0 = vessel.mass
+        m1 = m0 / math.exp(delta_v/Isp)
+        flow_rate = F / Isp
+        burn_time = (m0 - m1) / flow_rate
 
-        # Returning to kerbing
-        srf_altitude = conn.get_call(getattr, vessel.flight(), 'surface_altitude')
-        expr = conn.krpc.Expression.less_than(
-            conn.krpc.Expression.call(srf_altitude),
-            conn.krpc.Expression.constant_double(1000))
-        event = conn.krpc.add_event(expr)
-        with event.condition:
-            event.wait()
+        # Orientate ship
+        print('Orientating ship for circularization burn')
+        vessel.auto_pilot.reference_frame = node.reference_frame
+        vessel.auto_pilot.target_direction = (0, 1, 0)
+        vessel.auto_pilot.wait()
 
-        vessel.control.activate_next_stage()
+# Wait until burn
+        print('Waiting until circularization burn')
+        burn_ut = ut() + vessel.orbit.time_to_apoapsis - (burn_time/2.)
+# Execute burn
+        print('Ready to execute burn')
+        time_to_apoapsis = conn.add_stream(getattr, vessel.orbit, 'time_to_apoapsis')
+        while time_to_apoapsis() - (burn_time/2.) > 0:
+            pass
+        print('Executing burn')
+        vessel.control.throttle = 1.0
+        time.sleep(burn_time - 0.1)
+        print('Fine tuning')
+        vessel.control.throttle = 0.05
+        remaining_burn = conn.add_stream(node.remaining_burn_vector, node.reference_frame)
+        while remaining_burn()[1] > 0:
+            pass
+        vessel.control.throttle = 0.0
+        node.remove()
 
-        # Ending
-        while vessel.flight(vessel.orbit.body.reference_frame).vertical_speed < -0.1:
-            print('Altitude = %.1f meters' % vessel.flight().surface_altitude)
-            time.sleep(1)
-        print('Landed!')
+        print('Launch complete')
+        lead_time = 5
+        conn.space_center.warp_to(burn_ut - lead_time)
+
+
+
+
 
 if __name__== "__main__":
     spaceship = OrbitalSpaceship()
